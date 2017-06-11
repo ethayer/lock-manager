5589d709b239adecde15736dc5a2d058fc60350a
Erik Thayer <erik@thayer.io>
2017-06-11 12:48:41 -0500

    allow renaming app

--- smartapps/ethayer/lock-manager.src/lock-manager.groovy
+++ smartapps/ethayer/lock-manager.src/lock-manager.groovy
@@ -55,6 +55,7 @@ def mainPage() {
     section('Advanced', hideable: true, hidden: true) {
       input(name: 'overwriteMode', title: 'Overwrite?', type: 'bool', required: true, defaultValue: true, description: 'Overwrite mode automatically deletes codes not in the users list')
       input(name: 'enableDebug', title: 'Enable IDE debug messages?', type: 'bool', required: true, defaultValue: false, description: 'Show activity from Lock Manger in logs for debugging.')
+      label(title: 'Label this SmartApp', required: false, defaultValue: 'Lock Manager')
       paragraph 'Lock Manager © 2017 v1.4'
     }
   }
